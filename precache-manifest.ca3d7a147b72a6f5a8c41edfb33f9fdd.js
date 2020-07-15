fe162434c577bb9ff3bdb678f8e249bd891f78a7
Teja <tejpotharlanka@gmail.com>
2020-07-15 12:42:17 +0530

    Updates

---
+++ precache-manifest.ca3d7a147b72a6f5a8c41edfb33f9fdd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63e0b763c35b45ab420bca18eb2c9a6e",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "5d8c848a06b0f80a6d25",
+    "url": "/my-react-app/static/css/main.8e74b49f.chunk.css"
+  },
+  {
+    "revision": "8bd7fe290728c6baac68",
+    "url": "/my-react-app/static/js/2.709d63db.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.709d63db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d8c848a06b0f80a6d25",
+    "url": "/my-react-app/static/js/main.780f0320.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  },
+  {
+    "revision": "fcbb2552ba1cc2fb2819b90c3f0e43de",
+    "url": "/my-react-app/static/media/ReactJS.fcbb2552.jpg"
+  },
+  {
+    "revision": "630602c4f59aeacbced92175ccb7b2a4",
+    "url": "/my-react-app/static/media/apssdc_final.630602c4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
